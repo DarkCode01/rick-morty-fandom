52343c6910e7528d8300ce713ac244ee79613b6c
Jose (Darkcoder) <j0s3s3gur4@gmail.com>
2019-11-21 17:14:20 -0400

    ✅  Redesign navbar style to page... xD🤓

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import { Layout } from 'antd';
 
 // Components
-import Side from './components/sider';
+import Header from './components/header';
 
 // Sections Componets
 import Main from './pages/main';
@@ -15,25 +15,15 @@ class App extends Component {
     super(props);
 
     this.state = {}
-
-    this.handleToSelectSection = this.handleToSelectSection.bind(this);
-  }
-
-  handleToSelectSection(section) {
-    return () => {
-      this.setState(state => ({
-        ...state,
-        selected: section
-      }));
-    }
   }
 
   render() {
     return (
       <Layout style={{ minHeight: '100vh' }}>
-        <Side handle={this.handleToSelectSection} />
-        <Layout>
-          <BrowserRouter>
+        <BrowserRouter>
+          <Header />
+
+          <Layout>
             <Switch>
               <Route exact path="/" component={Main} />
               <Route exact path="/characters">
@@ -41,11 +31,11 @@ class App extends Component {
               </Route>
               <Route path="/characters/:id" component={Profile} />
             </Switch>
-          </BrowserRouter>
 
-          {/* Footer */}
-          <Layout.Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Layout.Footer>
-        </Layout>
+            {/* Footer */}
+            <Layout.Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Layout.Footer>
+          </Layout>
+        </BrowserRouter>
       </Layout>
     );
   }
